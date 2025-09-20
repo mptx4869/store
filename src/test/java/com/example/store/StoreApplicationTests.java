af9dc8024e0bc3a008d1e29ca753321c2b67d10d
mptx4869 <dangnhanhoa2305@gmail.com>
2025-09-20 23:16:40 +0700

    create the project

---
+++ src/test/java/com/example/store/StoreApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class StoreApplicationTests {
+
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@Test
+	void contextLoads() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/book", String.class);
+		assertThat(response.getBody()).isEqualTo("something");
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+}
